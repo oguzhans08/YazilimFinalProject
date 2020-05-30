a013c37ad3874f95561383d999aa0bbe0e4e3f9b
Oğuzhan Sağlık <45845015+oguzhans08@users.noreply.github.com>
2020-05-30 23:16:10 +0300

    Add files via upload

---
+++ RestWebServisOrnek/src/main/java/tr/edu/medipol/yazarac/RestWebServisOrnek/RestWebServis.java
@@ -0,0 +1,37 @@
+package tr.edu.medipol.yazarac.RestWebServisOrnek;
+
+ 
+
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+
+ 
+
+@RestController
+@RequestMapping(path="/ogrenci")
+public class RestWebServis {
+
+ 
+
+    private static List<Ogrenci> ogrenciListesi = new ArrayList<>();
+    static {
+        ogrenciListesi.add(new Ogrenci("Oğuzhan", 25));
+        ogrenciListesi.add(new Ogrenci("Hayriye", 21));
+        ogrenciListesi.add(new Ogrenci("Kerem", 23));
+    }
+    
+    @GetMapping("/listele")
+    public List<Ogrenci> listele(){
+        return ogrenciListesi;
+    }
+    @PostMapping("/ekle")
+    public Ogrenci Ekle(@RequestBody Ogrenci ogrenci){
+    	ogrenciListesi.add(ogrenci);
+		return ogrenci;
+    }
+    @PostMapping("/sil")
+    public  Ogrenci sil(@RequestBody Ogrenci ogrenci){
+        ogrenciListesi.add(ogrenci);
+        return ogrenci;
+    }
+}
---
+++ RestWebServisOrnek/src/test/java/tr/edu/medipol/yazarac/RestWebServisOrnek/Testbolum.java
@@ -0,0 +1,91 @@
+package tr.edu.medipol.yazarac.RestWebServisOrnek;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ogrenci")
+
+public class Testbolum {
+	// Kişileri Kontrol Testi
+    @Test
+    public void ListeKontrol() {
+        //Given  - Koşullar - Ön Şartlar
+    List<String> ogrenciler = new ArrayList<>();
+         {
+             String ogrenci1 ="Oğuzhan";
+             String ogrenci2 ="Hayriye";
+             String ogrenci3 ="Hüseyin";
+             int Not =25;
+            int Not2 = 21;
+            int Not3 =23;
+       //When
+
+        ogrenciler.add(ogrenci1+Not);
+        ogrenciler.add(ogrenci2+Not2);
+        ogrenciler.add(ogrenci3+Not3);
+
+      //Then - Kontrol
+            assertEquals("Oğuzhan",ogrenci1);
+            assertEquals("Hayriye",ogrenci2);
+            assertEquals("Hüseyin",ogrenci3);
+            assertEquals("Oğuzhan",ogrenci1);
+            assertEquals("Oğuzhan",ogrenci1);
+            assertEquals("Oğuzhan",ogrenci1);
+
+        }
+
+
+
+    }
+private void assertEquals(String string, String ogrenci1) {
+        // TODO Auto-generated method stub
+
+    }
+@GetMapping("/ogrenciEkle/{ogrenciAdi}")
+@Test
+//OgrenciEkleTesti
+//Given
+public void   OgrenciEkle() {
+    List<String> ogrenciler = new ArrayList<>();
+     {
+         String ogrenci1 ="Cenk";
+         String ogrenci2 ="Salim";
+         String ogrenci3 ="Özkan";
+     }
+     //When
+     String yeniogrenci="Kerem";
+     ogrenciler.add(yeniogrenci);
+     //Then
+     assertEquals(yeniogrenci,"Kerem");
+
+}
+@GetMapping("/ogrenciSil/{ogrenciAdi}")
+@Test
+public void OgrenciSil() {
+//OgrenciSil Testi
+    //Given
+        List<String> ogrenciler = new ArrayList<>();
+         {
+             String ogrenci1 ="Oğuzhan";
+             String ogrenci2 ="Hayriye";
+             String ogrenci3 ="Hüseyin";
+
+        //When
+
+             ogrenciler.remove(ogrenci3);
+
+        //Then
+             assertEquals(ogrenci3,"Hüseyin");
+         }
+
+
+}
+
+}
